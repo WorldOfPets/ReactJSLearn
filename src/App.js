d451244331294aefa6c608d1d5b3c61faca5567a
WorldOfPets <polivanov004@gmail.com>
2022-06-04 17:28:26 +0300

    first lesson react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+const App = () => {
+    return (
+      <div className="App">
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+      </div>
+    );
+}
+
+export default App;
